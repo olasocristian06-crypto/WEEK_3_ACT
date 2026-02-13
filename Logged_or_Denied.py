6d2d3b3501cffdf204236fc61a0b5a27b3811e54
Bob- <olaso.cristian06@gmail.com>
2026-02-13 14:15:57 +0800

    Logged_or_Denied.py

---
+++ Logged_or_Denied.py
@@ -0,0 +1,9 @@
+password = "123456"#YOUR STORED PASSWORD
+
+Userpass =input("ENTER PASSWORD: ")#INPUT YOUR PASSWORD
+
+if password == Userpass:
+    print("Logged In:)")#IF YOUR PASSWORD IS CORRECT
+    
+else:
+    print("Access Denied:(")#IF YOUR PASSWORD IS WRONG
